4fdbb179ad7fd1f13180e06c5a6f380b21767822
Viacheslav <sadney7@gmail.com>
2025-02-18 14:01:01 -0500

    Initialize front, back apps, containerize them

---
+++ src/MathEarn.API/Program.cs
@@ -0,0 +1,48 @@
+﻿using MathEarn.API.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Database connection string is missing!");
+
+Console.WriteLine($"CONNECTION STRING: {connectionString}");
+// Register database context with PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString)
+);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//Test Connection
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    try
+    {
+        var canConnect = dbContext.Database.CanConnect();
+        Console.WriteLine(canConnect ? "✅ Database connection established successfully!": "❌ Database connection failed to establish");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"❌ Database connection failed: {ex.Message}");
+    }
+}
+
+app.Run();
+
+
